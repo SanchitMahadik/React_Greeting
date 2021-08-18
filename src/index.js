d101447efc68dd2f4493390f664e6ed3f143654e
Sanchit <83035088+SanchitMahadik@users.noreply.github.com>
2021-08-18 16:34:45 +0530

    my first react app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+    <h1>Thapa Technical Netflix Pick</h1>
+    <p>List of Best Series</p>
+    <ol>
+      <li>Dark</li>
+      <li>Extra curricular</li>
+      <li>My Holo Love</li>
+      <li>My first-2 love</li>
+      <li>Mr Robot</li>
+    </ol>
+  </>,
+  document.getElementById('root')
+);
